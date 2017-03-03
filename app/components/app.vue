6a614493df3167c9b3693532ff5468ca98d3209a
johnmendez <john.a.mendez2012@gmail.com>
2017-03-02 18:14:08 -0600

    Html and Css Added to JS

--- app/components/app.vue
+++ app/components/app.vue
@@ -1,6 +1,70 @@
 <template lang="html">
-  <div class="">
-
+  <div class="app">
+    <header>
+      <nav>
+        <div class="nav-container">
+          <h1>Marvel</h1>
+        </div>
+      </nav>
+    </header>
+    <main>
+      <div class="sidebar">
+        <img src="http://placehold.it/250x250" alt="">
+        <h2>Chara Name</h2>
+        <h3>Chara Data</h3>
+        <h2 class="bottom-border">Creators</h2>
+        <ul>
+          <li>Name</li>
+          <li>Name</li>
+          <li>Name</li>
+          <li>Name</li>
+        </ul>
+      </div>
+      <div class="grid">
+        <div class="grid-item">
+          <h1 class="bottom-border">Characters</h1>
+          <div class="row">
+            <div class="row-item">
+              <img src="http://placehold.it/100x100" alt="">
+              <p>Chara Name</p>
+            </div>
+            <div class="row-item">
+              <img src="http://placehold.it/100x100" alt="">
+              <p>Chara Name</p>
+            </div>
+            <div class="row-item">
+              <img src="http://placehold.it/100x100" alt="">
+              <p>Chara Name</p>
+            </div>
+            <div class="row-item">
+              <img src="http://placehold.it/100x100" alt="">
+              <p>Chara Name</p>
+            </div>
+          </div>
+        </div>
+        <div class="grid-item">
+          <h1 class="bottom-border">Comics</h1>
+          <div class="row">
+            <div class="row-item">
+              <img src="http://placehold.it/100x100" alt="">
+              <p>Comic</p>
+            </div>
+            <div class="row-item">
+              <img src="http://placehold.it/100x100" alt="">
+              <p>Comic</p>
+            </div>
+            <div class="row-item">
+              <img src="http://placehold.it/100x100" alt="">
+              <p>Comic</p>
+            </div>
+            <div class="row-item">
+              <img src="http://placehold.it/100x100" alt="">
+              <p>Comic</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
   </div>
 </template>
 
